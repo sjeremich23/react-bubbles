f573409e145b7bf87c0ca7ae6a01029613a40671
sjeremich23 <51142646+sjeremich23@users.noreply.github.com>
2019-11-15 10:19:12 -0700

    Updated App.js and added .env file

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./scss/styles.scss";
 
 import Login from "./components/Login";
+import { PrivateRoute } from "./utils/PrivateRoute";
+import BubblePage from "./components/BubblePage";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <PrivateRoute
+            path="/bubblepage"
+            redirect="/"
+            component={BubblePage}
+          />
+        </Switch>
       </div>
     </Router>
   );
